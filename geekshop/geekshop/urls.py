4ff1d84b79796fe317eee7ee08608594a43c95e0
ochistov <chistov95@gmail.com>
2021-11-14 22:41:21 +0300

    repair urls.py

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from geekshop.mainapp.views import index, products
+from mainapp.views import index, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
